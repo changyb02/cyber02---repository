79a532f0f41185015ad381e4d6d135a287ed8248
changyb02 <2773718739@qq.com>
2024-09-25 19:19:06 +0800

    Update level1- 1.demo.c

--- level1- 1.demo.c
+++ level1- 1.demo.c
@@ -1,15 +1,15 @@
-//#include<stdio.h>
-//
-//int main()
-//{
-//	char str[100];
-//	int num, price, quan;
-//	scanf("%s %d %d %d", &str, &num, &price, &quan);
-//	printf("%d   ",num);
-//	for (int i = 1; i <= quan; i++)
-//	{
-//		printf("%s",str);
-//	}
-//	printf("   %d",price);
-//	return 0;
-//}
+#include<stdio.h>
+
+int main()
+{
+	char str[100];
+	int num, price, quan;
+	scanf("%s %d %d %d", &str, &num, &price, &quan);
+	printf("%d   ",num);
+for (int i = 1; i <= quan; i++)
+	{
+		printf("%s",str);
+	}
+	printf("   %d",price);
+	return 0;
+}
